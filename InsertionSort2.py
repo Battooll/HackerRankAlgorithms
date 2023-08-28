ae0010173eb34f058d182c8f5db2898e03879a00
Batool Hammoud <batoolhammoud1995@gmail.com>
2023-08-28 21:49:15 +0400

    Insertion Sort part 2

---
+++ InsertionSort2.py
@@ -0,0 +1,11 @@
+def insertionSort2(n, arr):
+    # Write your code here
+    for i in range(1, n):
+        for j in range(i):
+            if arr[j] > arr[i]:
+                arr[i], arr[j] = arr[j], arr[i]
+        print(*arr)
+
+
+arr=[1,3,4,5,2]
+insertionSort2(arr)
